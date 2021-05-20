8584149331b4fdad7ed93d0417719269ca9a5e74
TRSKZK <75508554+TRSKZK@users.noreply.github.com>
2021-05-20 12:51:26 +0300

    search form added

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -4,7 +4,7 @@ import styled  from 'styled-components'
 
 const StyledContainer = styled(MapContainer)`
 height:500px;
-width:700px;
+width:900px;
 
 
 `
@@ -15,11 +15,11 @@ const Map = () => {
     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <Marker position={[51.505, -0.09]}>
+  {/* <Marker position={[51.505, -0.09]}>
     <Popup>
       A pretty CSS3 popup. <br /> Easily customizable.
     </Popup>
-  </Marker>
+  </Marker> */}
  </StyledContainer>
     )
 }
---
+++ src/components/SearchForm.js
@@ -0,0 +1,47 @@
+import React,{useState} from 'react'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const SearchInput = styled.input`
+width:300px;
+height: 30px;
+border: #808080 solid 2px;
+border-radius: 20px;
+&:focus{
+    outline:none;
+}
+`
+
+const SubmitButton = styled.button`
+height: 30px;
+border:#808080 solid 2px;
+border-radius: 10px;
+margin: 0 0 0 10px;
+background-color: #ffff;
+
+
+`
+
+const Form = styled.form`
+max-width: 400px;
+margin: 30px 0 0 0;
+padding: 0 0 30px 0;
+`
+
+const SearchForm = () => {
+    const [country, setCountry] = useState(``);
+    return (
+        <Form >
+            <SearchInput type='text'
+                placeholder='Enter country name'
+                value={country}
+                onChange={ (e)=>setCountry(e.target.value)}/>
+            
+            <SubmitButton type="submit"><FontAwesomeIcon icon={faSearch}/></SubmitButton>
+        </Form >
+    )
+
+}
+
+export default SearchForm
